1c135f5df4c1391e556ec9bcd4cdefc3f13bac2b
Tiffany Chen <yuhan210@gmail.com>
2015-08-24 16:29:35 -0400

    check progress for all

--- tools/check-all-progress.py
+++ tools/check-all-progress.py
@@ -1,14 +1,33 @@
 import sys
+import os
 
 if __name__ == "__main__":
 
+	'''
 	if len(sys.argv) != 2:
 		print 'Usage:'. sys.argv[0], 'video_list'
 		exit(-1)
 	
-	video_list = open(sys.argv[1]).read().split()
-	start_idx = 1
-	end_idx = 1	
-
+	'''
 	
+	anno_path = '/var/www/amt/annos'
+	
+	n_keyframe = 0
+	for folder in os.listdir(anno_path):
+		if folder == 'README':
+			continue
 
+		n_keyframe += len(os.listdir(os.path.join(anno_path, folder)))
+
+
+	label_path = '/var/www/amt/labels'
+	
+	n_labeledframes = 0
+	for folder in os.listdir(label_path):
+		if folder == 'README':
+			continue
+		
+		n_labeledframes += len(os.listdir(os.path.join(label_path, folder)))
+
+	print 'Label progress'
+	print n_labeledframes, '/' , n_keyframe, (n_labeledframes/(n_keyframe* 1.0)) * 100
